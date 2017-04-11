58c8b72dd129420ea9137ae2411480026c7c87b2
zanjs <root@zanjs.com>
2017-04-11 16:50:43 +0800

    undefined

--- src/main/java/com/example/Decode.java
+++ src/main/java/com/example/Decode.java
@@ -1,7 +1,15 @@
 package com.example;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 
 @RestController
@@ -10,9 +18,46 @@ public class Decode {
     private DecoderService decoderService;
     private static final String FilePath = "E://code.jpg";
 
-    @RequestMapping("/decode")
+    @RequestMapping(value = "decode", method = RequestMethod.GET)
     public String decode(){
         return new DecoderService().decodeService(FilePath);
+    }
+
+    @RequestMapping(value = "decode", method = RequestMethod.POST)
+    public String post(@RequestParam("name") String name,
+                       @RequestParam("image") MultipartFile image,
+                       HttpServletRequest request) throws IOException {
+        if (image.isEmpty()) {
+            return "file is no find";
+        }
+
+        byte[] bytes = new byte[0];
+        try {
+            bytes = image.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String rootPath = request.getSession().getServletContext().getRealPath("/upload");
+
+        System.out.println("rootPath:"+rootPath);
+
+        BufferedOutputStream stream = null;
+        try {
+            stream = new BufferedOutputStream(new FileOutputStream(new File(rootPath + name)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (stream != null) {
+            stream.write(bytes);
+
+        }
+        assert stream != null;
+        stream.close();
+
+
+
+        return new DecoderService().decodeService(rootPath + name);
     }
 
 
